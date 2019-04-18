9e5f75b2034dafb3418e5d20588695f8782bfea8
RAJESH SHARMA <raj.sharma21@outlook.com>
2019-04-18 14:17:13 +1200

    Api done

--- client/components/News.jsx
+++ client/components/News.jsx
@@ -3,27 +3,29 @@ import { getNews } from "../API/news";
 
 export default class News extends React.Component {
   state = {
-    author: "",
-    title: "",
-    description: ""
+    news: []
   };
 
   componentDidMount() {
     getNews().then(res => {
-      const { author, title, description } = res;
       this.setState({
-        author,
-        title,
-        description
+        news: res
       });
     });
   }
   render() {
     return (
       <React.Fragment>
-        <h2>Author: {this.state.author}</h2>
-        <h2>Title: {this.state.title}</h2>
-        <p>Description: {this.state.description}</p>
+        <h2>News</h2>
+        {this.state.news.slice(0).map(article => {
+          return (
+            <div key={article.index}>
+              <h5>{article.author}</h5>
+              <h5>{article.title}</h5>
+              <p>{article.content}</p>
+            </div>
+          );
+        })}
       </React.Fragment>
     );
   }
